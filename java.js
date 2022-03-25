c6ecaa3be640210ab104fec8118f54478bd64b92
elOreo <tobias.rump@gmx.de>
2022-03-25 23:03:25 +0100

    Switch to actual project.

---
+++ java.js
@@ -0,0 +1,24 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+function generateFields() {
+    var number = output.innerHTML;
+    var renderWindow = document.getElementById("renderwindow");
+    var renderWindowWidth = renderWindow.offsetWidth;
+    var renderWindowHeight = renderWindow.offsetHeight;
+    
+    var fieldWidths = renderWindowWidth / number;
+
+    var fields = []; // add array element mit array.push("element")
+    
+    for(c = 0; c < number; c++){
+        
+    }
+    
+}
