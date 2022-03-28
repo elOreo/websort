7212445b948a46cd027c86414577df85f6a65915
elOreo <tobias.rump@gmx.de>
2022-03-28 12:49:36 +0200

    BubbleSort but not perfect

--- java.js
+++ java.js
@@ -7,6 +7,22 @@ output.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+function clearRenderWindow(){
+   //Check if there is an existing set and delete it.
+   if(document.getElementById("renderwindow").children.length > 0){
+    while (document.getElementById("renderwindow").firstChild) {
+      document.getElementById("renderwindow").removeChild(document.getElementById("renderwindow").firstChild);
+    }
+  }
+}
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
 
 function generateFields() {
     var number = output.innerHTML;
@@ -14,12 +30,7 @@ function generateFields() {
     var renderWindowWidth = renderWindow.offsetWidth;
     var renderWindowHeight = renderWindow.offsetHeight;
 
-    //Check if there is an existing set and delete it.
-    if(document.getElementById("renderwindow").children.length > 0){
-      while (document.getElementById("renderwindow").firstChild) {
-        document.getElementById("renderwindow").removeChild(document.getElementById("renderwindow").firstChild);
-      }
-    }
+    clearRenderWindow();
    
     var fieldWidths = Math.floor(renderWindowWidth / number -1);
     var fWidth = String(fieldWidths) + "px";
@@ -32,20 +43,52 @@ function generateFields() {
 
     for(c = 0; c < number; c++){
         var getRandomHeight = Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-        console.log(getRandomHeight);
         var field = document.createElement("div");
         field.className = "fieldObj";
         field.id = "f"+c;
         
-        if(number<21){
+        if(number<31){
           field.innerHTML = getRandomHeight;
         }
+        else{
+          field.innerHTML = ".";
+        }
+        
         fields.push(field);
         document.getElementById("renderwindow").appendChild(field);
         document.getElementById("f"+c).style.width = fWidth;
         document.getElementById("f"+c).style.height = getRandomHeight + "px";
-    }
+    }   
+}
 
-    
-    
+function bubbleSort(){
+  if(fields.length === 0){
+    return;
+  }
+  let len = fields.length;
+  let checked;
+  do{
+    checked = false;
+    //window.requestAnimationFrame();
+    for (let i= 0; i<len-1; i++){
+      if(fields[i].style.height > fields[i+1].style.height){
+        let tmp = fields[i];
+        fields[i] = fields[i+1];
+        fields[i+1] = tmp;
+        checked = true;
+        clearRenderWindow();
+        for(let j=0; j<len; j++){
+          
+          document.getElementById("renderwindow").appendChild(fields[j]);
+        }
+      }
+      
+    }
+  }while(checked);
+}
+
+function test(){
+  console.log("start");
+  sleep(2000);
+  console.log("fin");
 }
